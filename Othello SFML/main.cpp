3ed0ce898ce49c35cff96faf383f5fe5946d17db
codelazarus14 <codel1763@gmail.com>
2023-09-23 12:41:55 -0400

    added a few test pieces to the board

--- Othello SFML/main.cpp
+++ Othello SFML/main.cpp
@@ -1,24 +1,40 @@
 #include <SFML/Graphics.hpp>
 
-constexpr int g_boardScale = 100;
-constexpr int g_boardPadding = g_boardScale / 10;
+constexpr int g_boardScale{ 100 };
+constexpr float g_boardPadding{ g_boardScale / 10 };
+constexpr float g_squareOffset{ g_boardScale + g_boardPadding };
+
+static sf::Color othelloGreen{ 0, 128, 0, 255 };
+static std::vector<sf::RectangleShape> squares;
+static std::vector<sf::CircleShape> pieces;
+
+static void addPiece(sf::Color color, int posn) {
+	sf::CircleShape piece{ g_boardScale / 2 };
+	piece.setFillColor(color);
+	piece.setPosition(g_boardPadding + g_squareOffset * (posn % 8),
+					  g_boardPadding + g_squareOffset * (posn / 8));
+	pieces.push_back(piece);
+}
 
 int main() {
-	constexpr int dimensions{ 8 * (g_boardScale + g_boardPadding) + g_boardPadding };
-	constexpr int squareOffset{ g_boardScale + g_boardPadding };
-	sf::Color othelloGreen{ 0, 128, 0, 255 };
+	constexpr int dimensions{ static_cast<int>(8 * (g_boardScale + g_boardPadding) + g_boardPadding) };
 
 	sf::RenderWindow window(sf::VideoMode(dimensions, dimensions), "Othello");
-	std::vector <sf::RectangleShape> rects;
-	for (int i = 0; i < 64; i++) {
-		int xPos = g_boardPadding + squareOffset * (i % 8);
-		int yPos = g_boardPadding + squareOffset * (i / 8);
 
-		sf::RectangleShape square({g_boardScale, g_boardScale});
+	for (int i = 0; i < 64; i++) {
+		float xPos = g_boardPadding + g_squareOffset * (i % 8);
+		float yPos = g_boardPadding + g_squareOffset * (i / 8);
+
+		sf::RectangleShape square({ g_boardScale, g_boardScale });
 		square.setFillColor(othelloGreen);
 		square.setPosition(xPos, yPos);
-		rects.push_back(square);
+		squares.push_back(square);
 	}
+	// TODO: populate from othello struct
+	addPiece(sf::Color::White, 27);
+	addPiece(sf::Color::White, 36);
+	addPiece(sf::Color::Black, 28);
+	addPiece(sf::Color::Black, 35);
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -35,9 +51,8 @@ int main() {
 
 		window.clear();
 		// draw the board
-		for (int i = 0; i < rects.size(); i++) {
-			window.draw(rects[i]);
-		}
+		for (sf::RectangleShape square : squares) window.draw(square);
+		for (sf::CircleShape piece : pieces) window.draw(piece);
 		window.display();
 	}
 
