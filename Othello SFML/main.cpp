24926c17badfeca89064e6f2b8326680afc82aa8
codelazarus14 <codel1763@gmail.com>
2023-09-23 10:04:27 -0400

    board is drawn in window

--- Othello SFML/main.cpp
+++ Othello SFML/main.cpp
@@ -1,19 +1,43 @@
 #include <SFML/Graphics.hpp>
 
+constexpr int g_boardScale = 100;
+constexpr int g_boardPadding = g_boardScale / 10;
+
 int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
-	sf::CircleShape shape(100.0f);
-	shape.setFillColor(sf::Color::Green);
+	constexpr int dimensions{ 8 * (g_boardScale + g_boardPadding) + g_boardPadding };
+	constexpr int squareOffset{ g_boardScale + g_boardPadding };
+	sf::Color othelloGreen{ 0, 128, 0, 255 };
+
+	sf::RenderWindow window(sf::VideoMode(dimensions, dimensions), "Othello");
+	std::vector <sf::RectangleShape> rects;
+	for (int i = 0; i < 64; i++) {
+		int xPos = g_boardPadding + squareOffset * (i % 8);
+		int yPos = g_boardPadding + squareOffset * (i / 8);
+
+		sf::RectangleShape square({g_boardScale, g_boardScale});
+		square.setFillColor(othelloGreen);
+		square.setPosition(xPos, yPos);
+		rects.push_back(square);
+	}
 
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
+			switch (event.type) {
+			case sf::Event::KeyReleased:
+				if (event.key.scancode != sf::Keyboard::Scan::Escape)
+					break;
+			case sf::Event::Closed:
 				window.close();
+				break;
+			}
 		}
 
 		window.clear();
-		window.draw(shape);
+		// draw the board
+		for (int i = 0; i < rects.size(); i++) {
+			window.draw(rects[i]);
+		}
 		window.display();
 	}
 
